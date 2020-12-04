57cd7cedde65b0b523e2bff8b12debd5cfbcd48a
chris covarrubias <chris.covarrubias40@gmail.com>
2020-12-03 21:20:43 -0800

    workout

---
+++ routes/html.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const path = require("path")
+
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+// If no matching route is found default to home
+router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+
+module.exports = router;
